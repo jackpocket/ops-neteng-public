da749b4e15b9395737b44270a802010e4302e7d3
Vlad Bekker <vlad.bekker@jackpocket.com>
2022-04-06 11:07:31 -0400

    script changes - adding tcp check for google

--- snmp_poller/snmp_poller.sh
+++ snmp_poller/snmp_poller.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 
 #MANUAL STEPS#
-
+#sudo apt install git -y
 #sudo git clone https://github.com/jackpocket/ops-neteng.git
+
+
+#AUTOMATION
+#Set env
 export DD_SITE="datadoghq.com"
 export DD_AGENT_MAJOR_VERSION=7
 
 #Remove anything old or remaining
 sudo rm ookla*
 sudo apt remove datadog-agent -y
+
 #Run this install script
 #sudo apt-get update
 sudo apt install ssh -y
 sudo apt install curl -y
 sudo apt install snmp -y
-sudo apt isntall iputils-ping
+sudo apt install iputils-ping -y
 
-#Install Datadog Agent
+#Install Datadog Agent - hope this does not change
 curl -s https://s3.amazonaws.com/dd-agent/scripts/install_script.sh | bash /dev/stdin
 
 #Install relevant integrations
 sudo -u dd-agent datadog-agent integration install -t datadog-ping==1.0.2 -r
 sudo -u dd-agent datadog-agent integration install -t datadog-speedtest==1.0.0 -r
 
+#Install speedtest utility
 sudo wget https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-x86_64.tgz
 sudo tar -zxvf ookla-speedtest-1.1.1-linux-x86_64.tgz
 sudo cp speedtest* /usr/local/bin/
-
-#Press YES
-#sudo -u dd-agent speedtest
 
 #Must copy files now
 sudo cp ./snmp_poller/profiles/sophos-xg.yaml /etc/datadog-agent/conf.d/snmp.d/profiles/
@@ -36,14 +39,17 @@ sudo cp ./snmp_poller/profiles/brother.yaml /etc/datadog-agent/conf.d/snmp.d/pro
 sudo cp ./snmp_poller/profiles/zyxel.yaml /etc/datadog-agent/conf.d/snmp.d/profiles/
 sudo cp ./snmp_poller/conf/ping/conf.yaml /etc/datadog-agent/conf.d/ping.d/
 sudo cp ./snmp_poller/conf/speedtest/conf.yaml /etc/datadog-agent/conf.d/speedtest.d/
+sudo cp ./snmp_poller/conf/tcp_check/conf.yaml /etc/datadog-agent/conf.d/tcp_check.d/
 sudo cp ./snmp_poller/conf/snmp/conf.yaml /etc/datadog-agent/conf.d/snmp.d/
 
-
+#Enable and restart the agent
 sudo systemctl enable datadog-agent
 sudo systemctl restart datadog-agent
+
+#Cleanup
 sudo rm speedtest*
 sudo rm ookla*
 
-
-#Press YES
+#Press YES to accept agreement and run the test at least once
+#This must run in order for the check to succeed
 sudo -u dd-agent speedtest
