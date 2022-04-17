eb715f0c2cfde84ec1b50a2970c8098e7d40e06b
Vlad Bekker <vlad.bekker@jackpocket.com>
2022-04-17 00:45:46 -0400

    bug fixes, var names

--- snmp_poller/snmp_poller_installer_.py
+++ snmp_poller/snmp_poller_installer_.py
@@ -2,7 +2,7 @@ import os
 import fileinput
 import subprocess
 
-#from github import Github
+# from github import Github
 
 # Get DD environment variables
 dd_api_key = os.getenv('DD_API_KEY')
@@ -10,25 +10,25 @@ dd_agent_major_version = os.getenv('DD_AGENT_MAJOR_VERSION')
 dd_install_only = os.getenv('DD_INSTALL_ONLY')
 dd_site = os.getenv('DD_SITE')
 
-#Get JP environment variables
+# Get JP environment variables
 site = os.getenv('SITE')
 snmp_user = os.getenv('SNMP_USER')
 comm_string = os.getenv('COMM_STRING')
 auth_key = os.getenv('AUTH_KEY')
 priv_key = os.getenv('PRIV_KEY')
-firewall_ip = os.getenv('FIREWALL_IP') #LOCATE DEFAULT GW
+firewall_ip = os.getenv('FIREWALL_IP')  # LOCATE DEFAULT GW
 network_management_subnet = os.getenv('NETWORK_MANAGEMENT_SUBNET')
 upload_ftp_prod = os.getenv('UPLOAD_FTP_PROD')
 
-#Update - slow
-#os.system('sudo apt update')
+# Update - slow
+# os.system('sudo apt update')
 
-#disable ipv6
+# disable ipv6
 os.system('sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1')
 os.system('sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1')
 os.system('sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1')
 
-#Install some packages
+# Install some packages
 try:
     os.system('sudo apt install -y ssh')
     os.system('sudo apt install -y iputils-ping')
@@ -37,19 +37,19 @@ try:
 except:
     exit("Failed to install the packages")
 
-#Install Datadog Agent - hope this does not change
+# Install Datadog Agent - hope this does not change
 os.system('curl -s https://s3.amazonaws.com/dd-agent/scripts/install_script.sh | bash /dev/stdin')
 
-#Install relevant integrations
+# Install relevant integrations
 os.system('sudo -u dd-agent datadog-agent integration install -t datadog-ping==1.0.2 -r')
 os.system('sudo -u dd-agent datadog-agent integration install -t datadog-speedtest==1.0.0 -r')
 
-#Install speedtest utility
+# Install speedtest utility
 os.system('sudo wget https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-x86_64.tgz')
 os.system('sudo tar -zxf ookla-speedtest-1.1.1-linux-x86_64.tgz')
 os.system('sudo cp speedtest* /usr/local/bin/')
 
-#Copy templated files
+# Copy templated files
 os.system('sudo cp ./snmp_poller/profiles/sophos-xg.yaml /etc/datadog-agent/conf.d/snmp.d/profiles/')
 os.system('sudo cp ./snmp_poller/profiles/brother.yaml /etc/datadog-agent/conf.d/snmp.d/profiles/')
 os.system('sudo cp ./snmp_poller/profiles/zyxel.yaml /etc/datadog-agent/conf.d/snmp.d/profiles/')
@@ -59,15 +59,15 @@ os.system('sudo cp ./snmp_poller/conf/tcp_check/conf.yaml /etc/datadog-agent/con
 os.system('sudo cp ./snmp_poller/conf/snmp/conf.yaml /etc/datadog-agent/conf.d/snmp.d/')
 os.system('sudo cp ./snmp_poller/conf/agent/datadog.yaml /etc/datadog-agent/')
 
-#Give each file a variable
+# Give each file a variable
 dd_agent_conf = '/etc/datadog-agent/datadog.yaml'
 dd_snmp_conf = '/etc/datadog-agent/conf.d/snmp.d/conf.yaml'
 dd_ping_conf = '/etc/datadog-agent/conf.d/ping.d/conf.yaml'
 dd_speedtest_conf = '/etc/datadog-agent/conf.d/speedtest.d/conf.yaml'
 dd_tcp_check_conf = '/etc/datadog-agent/conf.d/tcp_check.d/conf.yaml'
 
-#Replace vars in files
-#with fileinput.FileInput(dd_snmp_conf, inplace=True, backup='.bak') as myfile:
+# Replace vars in files
+# with fileinput.FileInput(dd_snmp_conf, inplace=True, backup='.bak') as myfile:
 #
 # with fileinput.FileInput(dd_snmp_conf, inplace=True) as myfile:
 #     for line in myfile:
@@ -100,20 +100,22 @@ dd_tcp_check_conf = '/etc/datadog-agent/conf.d/tcp_check.d/conf.yaml'
 #         print(line.replace('<SITE>', site))
 
 
-subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/ping.d/conf.yaml", shell=False)
-subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/speedtest.d/conf.yaml", shell=False)
-subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/tcp_check.d/conf.yaml", shell=False)
-subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=False)
-subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/datadog.yaml", shell=False)
+# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/ping.d/conf.yaml")
+subprocess.call('sudo', 'sed', '-i', 's/<SITE>/', site, '/g', '/etc/datadog-agent/conf.d/ping.d/conf.yaml')
 
-subprocess.run("sudo sed -i \"s/<FIREWALL_IP>/${firewall_ip}/g\" /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
-subprocess.run("sudo sed -i s/<AUTH_KEY>/${auth_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
-subprocess.run("sudo sed -i s/<PRIV_KEY>/]${priv_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
-subprocess.run("sudo sed -i s/<COMMUNITY_STRING>/${comm_string}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
-subprocess.run("sudo sed -i s/<NETWORK_MANAGEMENT_SUBNET>/${network_management_subnet}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
-subprocess.run("sudo sed -i s/<SNMP_USER>/${snmp_user}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
-subprocess.run("sudo sed -i s/<UPLOAD_FTP_PROD>/${upload_ftp_prod}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
-
+# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/speedtest.d/conf.yaml", shell=False)
+# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/tcp_check.d/conf.yaml", shell=False)
+# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=False)
+# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/datadog.yaml", shell=False)
+#
+# subprocess.run("sudo sed -i \"s/<FIREWALL_IP>/${firewall_ip}/g\" /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
+# subprocess.run("sudo sed -i s/<AUTH_KEY>/${auth_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
+# subprocess.run("sudo sed -i s/<PRIV_KEY>/]${priv_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
+# subprocess.run("sudo sed -i s/<COMMUNITY_STRING>/${comm_string}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
+# subprocess.run("sudo sed -i s/<NETWORK_MANAGEMENT_SUBNET>/${network_management_subnet}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
+# subprocess.run("sudo sed -i s/<SNMP_USER>/${snmp_user}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
+# subprocess.run("sudo sed -i s/<UPLOAD_FTP_PROD>/${upload_ftp_prod}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
+#
 
 os.system('sudo systemctl enable datadog-agent')
 os.system('sudo systemctl restart datadog-agent')
@@ -124,11 +126,11 @@ os.system('sudo rm ookla*')
 # #This must run in order for the check to succeed
 os.system('sudo -u dd-agent speedtest')
 
-#os.system('sudo sed -i "s/<SITE>/$site/g" /etc/datadog-agent/conf.d/ping.d/conf.yaml')
+# os.system('sudo sed -i "s/<SITE>/$site/g" /etc/datadog-agent/conf.d/ping.d/conf.yaml')
 
-#sed -e "s/\${i}/1/" -e "s/\${word}/dog/" template.txt
+# sed -e "s/\${i}/1/" -e "s/\${word}/dog/" template.txt
 
-#WORK OUT SED
+# WORK OUT SED
 
 #
 # sudo sed -i "s/<SITE>/$site/g" /etc/datadog-agent/conf.d/ping.d/conf.yaml
@@ -144,16 +146,16 @@ os.system('sudo -u dd-agent speedtest')
 # sudo sed -i "s/<NETWORK_MANAGEMENT_VLAN>/$network_management_vlan/g" /etc/datadog-agent/conf.d/snmp.d/conf.yaml
 #
 
-#Replace tags with those variables for certain files
-#Site
-#community_string_v2
-#authkey_v3
-#privkey_v3
-#firewall_ip
-#printer_vlan
-#network_management_vlan
-#Second octet - future
-#Enable and restart the agent
+# Replace tags with those variables for certain files
+# Site
+# community_string_v2
+# authkey_v3
+# privkey_v3
+# firewall_ip
+# printer_vlan
+# network_management_vlan
+# Second octet - future
+# Enable and restart the agent
 #
 # sudo systemctl enable datadog-agent
 # sudo systemctl restart datadog-agent
@@ -173,27 +175,27 @@ os.system('sudo -u dd-agent speedtest')
 #
 
 
-#set secrets as env variables
-#I would set environment variables locally first and manually and pull them from somewhere (cloud vault/GCP/etc)
-#Add a script that will check for any updates to git branch repo currently checked out
+# set secrets as env variables
+# I would set environment variables locally first and manually and pull them from somewhere (cloud vault/GCP/etc)
+# Add a script that will check for any updates to git branch repo currently checked out
 # - specify the repo and the folder
-#check for SHA if it changed or string output and perform and re-run the poller script again
-#use python import git
-#use environment variables on the agent machines and leave them there locally.
-#How can they survive a reboot? - Linux ENV vars
+# check for SHA if it changed or string output and perform and re-run the poller script again
+# use python import git
+# use environment variables on the agent machines and leave them there locally.
+# How can they survive a reboot? - Linux ENV vars
 
-#Write the python script
+# Write the python script
 
-#import git
-#repo = git.Repo('Path/to/repo')
-#repo.remotes.origin.pull()
+# import git
+# repo = git.Repo('Path/to/repo')
+# repo.remotes.origin.pull()
 
-#Check if something changed:
+# Check if something changed:
 #
-#current = repo.head.commit
-#repo.remotes.origin.pull()
-#if current != repo.head.commit:
+# current = repo.head.commit
+# repo.remotes.origin.pull()
+# if current != repo.head.commit:
 #    print("It changed")
 ##
 
-#If changes are located, run the installer
+# If changes are located, run the installer
