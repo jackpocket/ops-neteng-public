cfdfa808c0a29964984d4d164616d6b80c17b982
Vlad Bekker <vlad.bekker@jackpocket.com>
2022-04-17 03:41:01 -0400

    adding puller for github

--- snmp_poller/github_pull.py
+++ snmp_poller/github_pull.py
@@ -3,7 +3,7 @@ import subprocess
 
 #	git config --global --add safe.directory /home/datadog/ops-neteng-public
 
-subprocess.Popen("cd", cwd="/home/datadog/ops-neteng-public")
+os.chdir("/home/datadog/ops-neteng-public")
 subprocess.call(["git", "pull"])
 
 
