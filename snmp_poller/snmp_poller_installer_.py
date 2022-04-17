a10dec07e8677298edd04b2c2344c200d307db47
Vlad Bekker <vlad.bekker@jackpocket.com>
2022-04-17 01:31:07 -0400

    bug fixes, var names

--- snmp_poller/snmp_poller_installer_.py
+++ snmp_poller/snmp_poller_installer_.py
@@ -99,20 +99,23 @@ dd_tcp_check_conf = '/etc/datadog-agent/conf.d/tcp_check.d/conf.yaml'
 #     for line in myfile:
 #         print(line.replace('<SITE>', site))
 
+#WORKS - callcheck but we do run below
+#subprocess.check_output(f'sudo sed -i s/SITE/{site}/g /etc/datadog-agent/conf.d/ping.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/SITE/{site}/g /etc/datadog-agent/conf.d/ping.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/SITE/{site}/g /etc/datadog-agent/conf.d/speedtest.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/SITE/{site}/g /etc/datadog-agent/conf.d/tcp_check.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/SITE/{site}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/SITE/{site}/g /etc/datadog-agent/datadog.yaml', shell=True)
 
-# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g" /etc/datadog-agent/conf.d/ping.d/conf.yaml")
-#subprocess.call(['sudo', 'sed', '-i', 's/<SITE>/', site, '/g', '/etc/datadog-agent/conf.d/ping.d/conf.yaml'])
-#subprocess.call(['sudo', 'sed', '-i', 's', '/', '<SITE>', '/', site, '/g', '/etc/datadog-agent/conf.d/ping.d/conf.yaml'])
+subprocess.run(f'sudo sed -i s/FIREWALL_IP/{firewall_ip}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/AUTH_KEY/{auth_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/PRIV_KEY/{priv_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/COMMUNITY_STRING/{comm_string}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/NETWORK_MANAGEMENT_SUBNET/{network_management_subnet}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/SNMP_USER/{snmp_user}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml', shell=True)
 
-#subprocess.check_output(f'"sed -i s/<SITE>/{python_var}/g /etc/datadog-agent/conf.d/ping.d/conf.yaml"', shell=False)
-subprocess.check_output(f'sudo sed -i s/SITE/{site}/g /etc/datadog-agent/conf.d/ping.d/conf.yaml', shell=True)
+subprocess.run(f'sudo sed -i s/UPLOAD_FTP_PROD/{upload_ftp_prod}/g /etc/datadog-agent/conf.d/tcp_check.d/conf.yaml', shell=True)
 
-# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/speedtest.d/conf.yaml", shell=False)
-# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/tcp_check.d/conf.yaml", shell=False)
-# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=False)
-# subprocess.run("sudo sed -i \"s/<SITE>/${site}/g\" /etc/datadog-agent/datadog.yaml", shell=False)
-#
-# subprocess.run("sudo sed -i \"s/<FIREWALL_IP>/${firewall_ip}/g\" /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
 # subprocess.run("sudo sed -i s/<AUTH_KEY>/${auth_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
 # subprocess.run("sudo sed -i s/<PRIV_KEY>/]${priv_key}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
 # subprocess.run("sudo sed -i s/<COMMUNITY_STRING>/${comm_string}/g /etc/datadog-agent/conf.d/snmp.d/conf.yaml", shell=True)
