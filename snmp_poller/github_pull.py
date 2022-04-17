f105911be47faa02343689ff582f47cea4a2ae28
Vlad Bekker <vlad.bekker@jackpocket.com>
2022-04-17 04:05:17 -0400

    adding puller for github

--- snmp_poller/github_pull.py
+++ snmp_poller/github_pull.py
@@ -12,8 +12,7 @@ if "Already up to date." in coolresult:
     print("No updates")
 else:
     print("Need need to execute the installer")
-    print("Cool update test")
-
+    os.system('python3 /home/datadog/ops-neteng-public/snmp_poller/snmp_poller_installer.py')
 
 #test
 
