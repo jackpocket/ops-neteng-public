772812b3305f6bdd43f1e0ddcf9993118cf4751b
Vlad Bekker <vlad.bekker@jackpocket.com>
2022-04-17 03:57:58 -0400

    adding puller for github

--- snmp_poller/github_pull.py
+++ snmp_poller/github_pull.py
@@ -3,12 +3,18 @@ import subprocess
 
 #	git config --global --add safe.directory /home/datadog/ops-neteng-public
 
-os.chdir("/home/datadog/ops-neteng-public")
-result, error, something = subprocess.check_output(["sudo", "git", "pull"])
-if "Already up to date." in result:
-    print("No updates")
-else:
-    print("Need need to execute the installer")
+# os.chdir("/home/datadog/ops-neteng-public")
+# result = subprocess.check_output(["sudo", "git", "pull"])
+
+p = subprocess.Popen("pwd", stdout=subprocess.PIPE)
+result = p.communicate()[0]
+print result
+
+
+# if "Already up to date." in result:
+#     print("No updates")
+# else:
+#     print("Need need to execute the installer")
 
 
 #test
