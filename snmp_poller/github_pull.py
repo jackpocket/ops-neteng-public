a3b289c72bd19bc201b6c6bb3dafc805fc1f7e09
Vlad Bekker <vlad.bekker@jackpocket.com>
2022-04-18 19:15:16 -0400

    changes to github  puller

--- snmp_poller/github_pull.py
+++ snmp_poller/github_pull.py
@@ -9,7 +9,7 @@ result = subprocess.check_output(["sudo", "git", "pull"])
 coolresult = (str(result))
 
 if "Already up to date." in coolresult:
-    print("No updates")
+    pass
 else:
-    print("Need need to execute the installer")
+    #print("Need need to execute the installer")
     os.system('python3 /home/datadog/ops-neteng-public/snmp_poller/snmp_poller_installer.py')
